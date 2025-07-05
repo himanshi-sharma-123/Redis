2527dbded4fc61fa1f718da88f56cd0df173d43a
Himanshi Sharma <himanshish456@gmail.com>
2025-07-05 21:47:08 +0530

    redis configuration, model and repositories

---
+++ src/main/java/org/example/model/Employee.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String department;
+}
